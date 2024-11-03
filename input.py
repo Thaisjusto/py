5d5810f5426029b2fb282c480b7dc88b64a3d339
Thaís Justo <104525270+Thaisjusto@users.noreply.github.com>
2024-11-03 14:41:12 -0300

    Update input.py

--- input.py
+++ input.py
@@ -1,4 +1,4 @@
-# x = int(input('Digite um numero:'))
-# y = int(input('Digite mais um numero:'))
-# z = x + y
-# print(f'a soma dos dois valores é {z}')
+ x = int(input('Digite um numero:'))
+ y = int(input('Digite mais um numero:'))
+ z = x + y
+print(f'a soma dos dois valores é {z}')
