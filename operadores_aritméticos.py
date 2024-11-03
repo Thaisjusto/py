206aa4cf8ae76ef8aa10f0c9f67b8b4e2d8eb094
Thaís Justo <104525270+Thaisjusto@users.noreply.github.com>
2024-11-03 14:42:21 -0300

    Update operadores_aritméticos.py

--- operadores_aritméticos.py
+++ operadores_aritméticos.py
@@ -1,10 +1,10 @@
-# a = 40
-# c = 3
-# r = a * c
-# print(r)
+a = 40
+c = 3
+r = a * c
+print(r)
 
 # operadores aritméticos 
-# x = 5
-# y = 26
-# z = x + y 
-# print(f'a soma dos dois valores é {z}')
+x = 5
+y = 26
+z = x + y 
+print(f'a soma dos dois valores é {z}')
