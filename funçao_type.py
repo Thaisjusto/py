ca0bd098667f25cd00cc65a58d98f8fe434abb94
Thaís Justo <104525270+Thaisjusto@users.noreply.github.com>
2024-11-03 14:39:55 -0300

    Update funçao_type.py

--- funçao_type.py
+++ funçao_type.py
@@ -1,12 +1,12 @@
-# função type
-# print(type(media))
-# print(type(n2))
-# print(type(nome))
-# print(type(estado))
-# print(type(1+2j))
+função type
+print(type(media))
+print(type(n2))
+print(type(nome))
+print(type(estado))
+print(type(1+2j))
 
-# função isinstance() faz uma verificação e retorna true or false
+função isinstance() faz uma verificação e retorna true or false
 
-# a = 10
-# b = 'sol'
-# print(isinstance(a,(int, float))) 
+a = 10
+b = 'sol'
+print(isinstance(a,(int, float))) 
