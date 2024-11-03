539f0d2392e13b92f93c034dc375a4cc7d11407a
Thaís Justo <104525270+Thaisjusto@users.noreply.github.com>
2024-11-03 14:11:59 -0300

    Update laços_encadeados.py

--- laços_encadeados.py
+++ laços_encadeados.py
@@ -1,9 +1,10 @@
-# for cont_ex in range (1,6): #nos parenteses esta a contagem
-#     print(f'\nRodada: {cont_ex}') #\n para qubrar uma linha
-#     for cont_in in range (5, 0, -1): #-1 esta fazendo um decremento
-#         print(f'Valor: {cont_in}')
+for cont_ex in range (1,6): #nos parenteses esta a contagem
+print(f'\nRodada: {cont_ex}') #\n para qubrar uma linha
+for cont_in in range (5, 0, -1): #-1 esta fazendo um decremento
+         print(f'Valor: {cont_in}')
+print('fim dos laços')
 
-# print('fim dos laços')
+----------------------------------------------------
 
 import random
 
