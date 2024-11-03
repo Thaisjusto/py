1a69c4b71cb729790b11c0b13050cbe80ed21d52
Thaís Justo <104525270+Thaisjusto@users.noreply.github.com>
2024-11-03 14:40:44 -0300

    Update if_elif_else.py

--- if_elif_else.py
+++ if_elif_else.py
@@ -1,12 +1,12 @@
-# n1 = float(input("Digite sua primeira nota:"))
-# n2 = float(input("Digite sua segunda nota:"))
+n1 = float(input("Digite sua primeira nota:"))
+n2 = float(input("Digite sua segunda nota:"))
 
-# media = (n1 + n2) / 2 
+media = (n1 + n2) / 2 
 
-# if media >= 7:
-#     print('Aprovado!')
-# elif media >= 5:
-#     print('Você esta em recuperação :(')
-# else:
-#     print('Reprovado! Até ano que vem otário hihihi')
+if media >= 7:
+    print('Aprovado!')
+elif media >= 5:
+   print('Você esta em recuperação :(')
+else:
+   print('Reprovado! Até ano que vem otário hihihi')
 
