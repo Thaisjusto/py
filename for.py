91ac0546eca635dd2e38e0e8ea6b21d9184b1c70
Thaís Justo <104525270+Thaisjusto@users.noreply.github.com>
2024-11-03 14:19:40 -0300

    Update for.py

--- for.py
+++ for.py
@@ -1,11 +1,11 @@
-# lista = [2,6,9,3,12,7,10]
-# for item in lista:
-#     print(item)
-# for numero in range(1,11):
-#     print(numero)
+lista = [2,6,9,3,12,7,10]
+ for item in lista:
+    print(item)
+ for numero in range(1,11):
+     print(numero)
 
-# for x in range(2, 20, 2):
-#     print(x)
+ for x in range(2, 20, 2):
+     print(x)
 
 pedras = ('rubi', 'esmeralda', 'quartzo', 'safira', 'diamante', 'turmalina')
 
